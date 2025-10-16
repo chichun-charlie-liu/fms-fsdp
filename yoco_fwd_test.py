97e3e8b1b7cea4a10c0935dff4a518cbd4978dd4
cliu-us <cliu@us.ibm.com>
2025-10-16 16:03:20 -0400

    add exam_inp for debug
    Signed-off-by: cliu-us <cliu@us.ibm.com>

--- yoco_fwd_test.py
+++ yoco_fwd_test.py
@@ -18,3 +18,15 @@ with torch.no_grad():
 
 print(output)
 
+# test 2, use previously saved data and labels to calc loss
+zl_coeff: float = 1e-4
+exam_inp = torch.load("exam_inp.pt")
+inputs = exam_inp["inputs"][:1].to("cuda")  # use one batch of data
+label = exam_inp["labels"][:1].to("cuda")
+
+output = model(inputs)
+ce_loss = torch.nn.CrossEntropyLoss()
+loss = ce_loss(output.view(-1, output.size(-1)), label.view(-1).long())
+loss = loss + zl_coeff * torch.logsumexp(output, dim=-1).pow(2).mean()
+print(loss)
+# print(model.clip_grad_norm_(1.0).item())
